95c29a87ded6d52dc9d863355673ba464e630a30
Calvin RIce <ramen.calvin@yahoo.com>
2023-04-04 14:41:46 -0500

    epic

--- ContentView.swift
+++ ContentView.swift
@@ -1,18 +1,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var answer = Int
+    @State var answer: Int? = nil
     @State var number1: Int? = nil
     @State var number2: Int? = nil
     
     var body: some View {
-        HStack {
-            TextField("Enter Number 1", value: $number1, format: .number)
-            TextField("Enter Number 2", value: $number1, format: .number)
         VStack {
-            Button("Multiply"){
-                Text(answer)
+            TextField("Enter Number 1", value: $number1, format: .number)
+            
+            TextField("Enter Number 2", value: $number1, format: .number)
+            
+            Button("Multiply") {
+                
             }
+            
+            Text("answer")
         }
     }
 }
