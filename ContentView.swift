6f133ce926b008631f2d634b2e88f7f15e4fd85d
Dylan Domeracki <ddomeracki4103@Stu.d214.org>
2023-04-04 14:40:49 -0500

    Commit3

--- ContentView.swift
+++ ContentView.swift
@@ -1,17 +1,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var answer = Int
+    @State var answer : Int
     @State var number1: Int? = nil
     @State var number2: Int? = nil
     
     var body: some View {
-        HStack {
             TextField("Enter Number 1", value: $number1, format: .number)
             TextField("Enter Number 2", value: $number1, format: .number)
-        VStack {
             Button("Multiply"){
-                Text(answer)
+                
+            }
+            Button("Add"){
+                
+            }
+            Button("Subtract"){
+                
+            }
+            Button("Divide"){
+                
             }
         }
     }
